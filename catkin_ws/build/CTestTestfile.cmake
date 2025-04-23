26e0b4d80c90db7fd40ad456c77544728aeb9ebd
Lokman <121123150+Lokmanorkut@users.noreply.github.com>
2025-04-23 08:07:04 +0300

    Add files via upload

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,24 @@
+# CMake generated Testfile for 
+# Source directory: /home/alchemist/catkin_ws/src
+# Build directory: /home/alchemist/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("labirent_ortam")
+subdirs("basit_uygulamalar")
+subdirs("robot_gazebo")
+subdirs("slam")
+subdirs("turtlebot3/turtlebot3")
+subdirs("turtlebot3_msgs")
+subdirs("turtlebot3/turtlebot3_navigation")
+subdirs("turtlebot3_simulations/turtlebot3_simulations")
+subdirs("ogretici_paket")
+subdirs("turtlebot3/turtlebot3_bringup")
+subdirs("turtlebot3/turtlebot3_example")
+subdirs("turtlebot3_simulations/turtlebot3_fake")
+subdirs("turtlebot3_simulations/turtlebot3_gazebo")
+subdirs("turtlebot3/turtlebot3_slam")
+subdirs("turtlebot3/turtlebot3_teleop")
+subdirs("robot_description")
+subdirs("turtlebot3/turtlebot3_description")
